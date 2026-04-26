2a499ad516c601254378a54914ec8063a1926baa
Jacky He <hekinghung@gmail.com>
2026-04-26 20:53:35 +0100

    ci: add bencodepy test case

--- test/test_readme_examples.py
+++ test/test_readme_examples.py
@@ -1,5 +1,9 @@
 """Deterministic checks for README examples and public API imports."""
 
+import hashlib
+
+import bencodepy
+
 from torrentlib import Peer, Torrent, TorrentStatus
 from torrentlib.Tracker import Check, Query, TorrentStatus as TrackerTorrentStatus
 from torrentlib.Tracker.TrackerQueryException import TrackerQueryException
@@ -37,6 +41,29 @@ def test_explicit_torrent_creation_matches_readme():
     assert torrent.name == "example_file.iso"
     assert torrent.event is TorrentStatus.STOPPED
     assert torrent.left == 1145141919810
+
+
+def test_ubuntu_torrent_file_parsing_uses_bencodepy(tmp_path):
+    info = {
+        b"name": b"ubuntu-24.04-desktop-amd64.iso",
+        b"piece length": 262144,
+        b"length": 1024,
+        b"pieces": hashlib.sha1(b"ubuntu-piece").digest(),
+    }
+    torrent_dict = {
+        b"announce": b"https://torrent.ubuntu.com/announce",
+        b"info": info,
+    }
+    torrent_path = tmp_path / "ubuntu.torrent"
+    torrent_path.write_bytes(bencodepy.encode(torrent_dict))
+
+    torrent = Torrent.from_file(str(torrent_path))
+
+    assert torrent.name == "ubuntu-24.04-desktop-amd64.iso"
+    assert torrent.total_size == 1024
+    assert torrent.piece_length == 262144
+    assert torrent.num_pieces == 1
+    assert torrent.metadata is not None
 
 
 def test_tracker_check_api_matches_readme():
